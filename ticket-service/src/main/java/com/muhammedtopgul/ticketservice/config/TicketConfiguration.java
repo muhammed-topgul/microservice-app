81b5e1b7d17fae375353801ee76dea452c5fe7ee
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-12-13 22:52:01 +0300

    elasticsearch bugfix

--- ticket-service/src/main/java/com/muhammedtopgul/ticketservice/config/TicketConfiguration.java
+++ ticket-service/src/main/java/com/muhammedtopgul/ticketservice/config/TicketConfiguration.java
@@ -2,6 +2,7 @@ package com.muhammedtopgul.ticketservice.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories("com.muhammedtopgul")
 @EnableElasticsearchRepositories
+@ComponentScan("com.muhammedtopgul")
 public class TicketConfiguration {
 
     @Bean
